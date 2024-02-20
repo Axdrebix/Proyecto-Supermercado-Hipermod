8ec23a1d0eb69db9405d324de26e5fd34261efab
Axdrebix <120673742+Axdrebix@users.noreply.github.com>
2024-02-19 21:14:31 -0400

    Proyecto_1.1.cpp

--- Proyecto_1.1.cpp
+++ Proyecto_1.1.cpp
@@ -291,11 +291,11 @@ void menu(int& eleccion)
 /*		MENU OPCION (CREDITOS)				*/
 void Creditos(){
     cout << "\n\nCreadores: "<<endl;
-    cout << "Francisco 			CI: V-31.770.825" << endl;
-    cout << "Manuel Pastrano 	CI: V-28.534.779" << endl;
-    cout << "Carlos Hernandez 	CI: V-31.074.002" << endl;
-    cout << "Andres Gomez 		CI: V-31.577.117" << endl;
-    cout << "Ernesto Balbás 	CI: V-30.932.082" << endl;
+    cout << "Francisco Fonseca  CI: V-26.359.537" << endl;
+    cout << "Manuel Pastrano    CI: V-28.534.779" << endl;
+    cout << "Carlos Hernandez   CI: V-27.975.753" << endl;
+    cout << "Andres Gomez       CI: V-31.085.717" << endl;
+    cout << "Ernesto Balbas     CI: V-30.932.082" << endl;
 
     //cout << "\nPresiona cualquier tecla para salir. ";
     system("pause");
